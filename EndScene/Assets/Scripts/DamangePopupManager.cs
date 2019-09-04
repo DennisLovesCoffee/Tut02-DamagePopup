1ee25d95fadc64a942a8fb1684ffd26c5e91fa42
Dennis <vymerd@gmail.com>
2019-09-04 20:49:14 +0200

    Start and End scene

---
+++ EndScene/Assets/Scripts/DamangePopupManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamangePopupManager : MonoBehaviour
+{
+    #region Singleton
+
+    public static DamangePopupManager instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Debug.Log("We messed UP!!");
+        }
+    }
+
+    #endregion
+
+
+    [SerializeField]
+    private GameObject damagePopupPrefab;
+
+
+    public void DisplayDamagePopup(int amount, Transform popupParent)
+    {
+        GameObject damagePopup = Instantiate(damagePopupPrefab, popupParent.transform.position, Quaternion.identity, popupParent);
+        damagePopup.GetComponent<DamagePopup>().SetUp(amount);
+    }
+}
---
+++ EndScene/Assets/Scripts/Health.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform damagePopupTransform;
+
+    public void TakeDamage(int amount)
+    {
+        DamangePopupManager.instance.DisplayDamagePopup(amount, damagePopupTransform);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            TakeDamage(Random.Range(10,25));
+        }
+    }
+}
